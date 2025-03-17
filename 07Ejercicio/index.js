1d4ff45cee1ffcc2ba47b35172b24970fa3fe8bc
Sandra Furet Díaz <sanfurdia@alu.edu.gva.es>
2025-03-17 15:55:36 +0100

    terminada v7

--- 07Ejercicio/index.js
+++ 07Ejercicio/index.js
@@ -1,10 +1,15 @@
 import { Inicio } from "./components.js";
 import { Layout } from "./components.js";
-import { Contacto } from "./components.js";  // Asegúrate de importar Contacto
+import { Contacto } from "./components.js"; 
+import { Tarjeta } from "./components.js"; 
+//import { Tarejat } from "./components.js";
 
 const routes = {
   "/Inicio": { view: () => m(Layout,[m(Inicio)]) },
   "/Contacto": { view: () =>  m(Layout,[m(Contacto)]) }, // Nueva ruta para la página de contacto
+  "/Tarjeta/:id": { view: () => m(Layout, [m(Tarjeta)]) },// Nueva ruta para la página de contacto
+ // "/Tarjeta":{view:()=>m(Layout,[m(Tarjeta,{id})])},
 };
 
 m.route(document.body, "/Inicio", routes);
+
